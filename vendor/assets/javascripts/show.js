e39992fe9537aca0f7db6c40ff78038982985727
Thien NNT <nguyennhutamthienbk2011@gmail.com>
2015-11-10 12:29:01 +0700

    both maps work well

--- vendor/assets/javascripts/show.js
+++ vendor/assets/javascripts/show.js
@@ -1,20 +1,24 @@
 $(document).ready(function(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: new google.maps.LatLng(latitude, longitude),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+  function show(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: new google.maps.LatLng(latitude, longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+    
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latitude, longitude),
+        map: map
+      });
+    var infowindow = new google.maps.InfoWindow();
   
-  var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(latitude, longitude),
-      map: map
-    });
-  var infowindow = new google.maps.InfoWindow();
-
-  google.maps.event.addListener(marker, 'click', (function(marker) {
-        return function() {
-          infowindow.setContent(address);
-          infowindow.open(map, marker);
-        }
-      })(marker));
+    google.maps.event.addListener(marker, 'click', (function(marker) {
+          return function() {
+            infowindow.setContent(address);
+            infowindow.open(map, marker);
+          }
+        })(marker));
+  };
+  if (choose == 0)
+    show();
 });
